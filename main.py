e17b680b0354c04a0cc7bd811ecc306d4cc6b908
SuryaPrakash4477 <71883857+SuryaPrakash4477@users.noreply.github.com>
2024-02-09 13:48:14 +0530

    Some more improvement

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 import requests
 from send_email import send_email
 
-url = "https://newsapi.org/v2/everything?q=tesla&" \
+topic = "tesla"
+
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
       "from=2024-01-09&sortBy=publishedAt&apiKey=" \
-      "0f386d02df7849d9a94be18d4aaf12cc"
+      "0f386d02df7849d9a94be18d4aaf12cc" \
+      "&langauge=en"
 # Passing the keuy of API
 api_key = "0f386d02df7849d9a94be18d4aaf12cc"
 request = requests.get(url)
 content = request.json()
 
 body = ""
-for article in content["articles"]:
-      body = body + str(article["title"]) + "\n" + str(article["description"]) + 2*"\n"
+for article in content["articles"][:20]:
+      body = "Subject: Today's News" + \
+            "\n" + body + str(article["title"]) + "\n" \
+             + str(article["description"]) \
+             + "\n" + article["url"] + 2*"\n"
 
 body = body.encode("utf-8")
 send_email(body)
